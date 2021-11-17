68262fd15c18537cabef2b866cb59a9aac56b422
Abhishek Tomar <61470005+Thakur-15x@users.noreply.github.com>
2021-11-17 12:46:15 +0530

    new commit

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+//  home = document.getElementById("home")
+//  skill = document.getElementById("skill")
+//  textbox = document.getElementsByClassName('.text-box')
+//  box = document.getElementsByClassName(".box")
+
+// home.addEventListener('onclick', ()=>{
+//     textbox.style.display='block';
+//     box.style.display='none';
+    ;
+    // if (textbox.style.display=='none') {
+    //     .style.display='block'      
+    // } else{
+    //     home.style.display='block'
+    //     skill.style.display='none'
+    // }
+
+// });
+// skill.addEventListener('onclick', ()=>{
+//     if (home.style.display=='none') {
+//         skill.style.display='block'      
+//     } else{
+//         home.style.display='none'
+//         skill.style.display='block'
+//     }
+
+// });
 var typed = new Typed(".auto", {
-    strings: [" Abhishek Tomar ", "a Web Developer", "a UI Designer"],
+    strings: [" Abhishek Tomar ", "Front-End Developer", "a UI Designer"],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
