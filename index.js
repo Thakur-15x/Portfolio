30b52f27912c91346f5f438e23e962096bf62f62
Abhishek Tomar <61470005+Thakur-15x@users.noreply.github.com>
2021-11-18 14:02:24 +0530

    update

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 function myfun() {
     document.getElementById("box").style.display = "block";
-    document.getElementById("user").style.display = "block";
+    // document.getElementById("user").style.display = "none";
+    document.getElementById("box4").style.display = "none";
     document.getElementById("box1").style.display = "none";
 
 }
 function myfun1() {
     document.getElementById("box").style.display = "none";
-    document.getElementById("box1").style.display = "none";
-    document.getElementById("user").style.display = "none";
+    document.getElementById("box1").style.display = "block";
+    document.getElementById("box4").style.display = "none";
+    // document.getElementById("user").style.display = "none";
 
 }
 function myfun2() {
     document.getElementById("box").style.display = "none";
     document.getElementById("box1").style.display = "none";
-    document.getElementById("user").style.display = "none";
+    document.getElementById("box4").style.display = "none";
+    // document.getElementById("user").style.display = "none";
 
 }
 function myfun3() {
     document.getElementById("box").style.display = "none";
-    document.getElementById("box1").style.display = "block";
-    document.getElementById("user").style.display = "none";
+    document.getElementById("box1").style.display = "none";
+    document.getElementById("box4").style.display = "block";
+    // document.getElementById("user").style.display = "none";
 
 }
 var typed = new Typed(".auto", {
@@ -27,4 +31,17 @@ var typed = new Typed(".auto", {
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
+})
+var fom = document.getElementById('fom');
+
+fom.addEventListener('mousemove', (e) =>{
+    var x = (window.innerWidth / 2 - e.pageX) / 12;
+    var y = (window.innerHeight / 2 - e.pageY) / 12;
+
+    fom.style.transform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
+
+});
+
+fom.addEventListener('mouseleave', function(){
+    fom.style.transform = 'rotateX(0deg) rotateY(0deg)';
 })
